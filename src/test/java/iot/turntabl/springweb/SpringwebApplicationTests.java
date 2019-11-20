fa557858fb6371a141fc85688dbb27a5421d6cc0
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-20 11:13:31 +0000

    spring web init

---
+++ src/test/java/iot/turntabl/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package iot.turntabl.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
