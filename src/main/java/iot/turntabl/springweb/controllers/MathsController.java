6a5349050ae6e836efeb968d5594bda0db2e4ae2
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-20 16:00:10 +0000

    docs written for maths controller with swagger

--- src/main/java/iot/turntabl/springweb/controllers/MathsController.java
+++ src/main/java/iot/turntabl/springweb/controllers/MathsController.java
@@ -1,17 +1,22 @@
 package iot.turntabl.springweb.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import iot.turntabl.springweb.models.Number;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class MathsController {
     @Autowired
     private Maths ops;
 
-    @RequestMapping("/add")
+    @ApiOperation("Adds two numbers passed as route parameters and returns the result")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Number add(
             @RequestParam(value = "num1") String num1,
             @RequestParam(value = "num2") String num2){
@@ -20,7 +25,8 @@ public class MathsController {
         return new Number(this.ops.add(number1, number2));
     }
 
-    @RequestMapping("/subtract")
+    @ApiOperation("Subtracts two numbers passed as parameters to route and returns the result")
+    @RequestMapping(value = "/subtract", method = RequestMethod.GET)
     public Number subtract(
             @RequestParam(value = "num1") String num1,
             @RequestParam(value = "num2") String num2){
